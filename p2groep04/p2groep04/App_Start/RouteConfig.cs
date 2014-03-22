f66da3e98ad26407ee283dd7a36e8a7c90d72e61
208134ld <l.dupont@hotmail.be>
2014-03-22 20:28:29 +0100

    Controller + 2 formulieren toegevoegd (moet nog verder afgewerkt worden)

--- p2groep04/p2groep04/App_Start/RouteConfig.cs
+++ p2groep04/p2groep04/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace p2groep04
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "LoginForm", id = UrlParameter.Optional }
             );
         }
     }
---
+++ p2groep04/p2groep04/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using p2groep04.Models;
+
+
+namespace p2groep04.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ViewResult LoginForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult LoginForm(LogInResponse guestResponse)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult ForgotPasswordForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult ForgotPasswordForm(LogInResponse logInResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: Email to the party organizer
+                return View("Thanks", logInResponse);
+            }
+            else
+            {
+                //there is a validation error
+                return View();
+            }
+        }
+    }
+}
