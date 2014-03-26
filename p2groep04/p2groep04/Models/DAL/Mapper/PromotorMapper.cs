67e0f36471027a019a1254eef3f5e98db559fa8e
BillUnboxedCrate <bram.baert@hotmail.com>
2014-03-26 19:38:10 +0100

    Mappers voor BPCoordinator, Promotor en Student aangemaakt

---
+++ p2groep04/p2groep04/Models/DAL/Mapper/PromotorMapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace p2groep04.Models.DAL.Mapper
+{
+    public class PromotorMapper:EntityTypeConfiguration<Promotor>
+    {
+        public PromotorMapper()
+        {
+            HasKey(p => p.Id);
+            Property(p => p.FirstName).IsRequired().HasMaxLength(50);
+            Property(p => p.LastName).IsRequired().HasMaxLength(50);
+            Property(p => p.Email).IsRequired();
+            Property(p => p.Password).IsRequired();
+        }
+           
+    }
+}
---
+++ p2groep04/p2groep04/Models/DAL/Mapper/StudentMapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace p2groep04.Models.DAL.Mapper
+{
+    public class StudentMapper : EntityTypeConfiguration<Student>
+    {
+        public StudentMapper()
+        {
+            HasKey(s => s.Id);
+            Property(s => s.FirstName).IsRequired().HasMaxLength(50);
+            Property(s => s.LastName).IsRequired().HasMaxLength(50);
+            Property(s => s.Email).IsRequired();
+            Property(s => s.Password).IsRequired();
+            HasOptional(s => s.Suggestion).WithRequired().Map(su => su.MapKey("suggestion_id")).WillCascadeOnDelete(false);
+        }
+    }
+}
