28141dfdd10958d0dfd4324a15769177e4bc1cd1
Roy Hollanders <roy_9852@hotmail.com>
2014-03-27 10:11:06 +0100

    UserMapper en StudentMapper aangemaakt

--- p2groep04/p2groep04/Models/DAL/Mapper/StudentMapper.cs
+++ p2groep04/p2groep04/Models/DAL/Mapper/StudentMapper.cs
@@ -7,16 +7,11 @@ using p2groep04.Models.Domain;
 
 namespace p2groep04.Models.DAL.Mapper
 {
-    public class StudentMapper : EntityTypeConfiguration<Student>
+    public class StudentMapper: EntityTypeConfiguration<Student>
     {
         public StudentMapper()
         {
-            HasKey(s => s.Id);
-            Property(s => s.FirstName).IsRequired().HasMaxLength(50);
-            Property(s => s.LastName).IsRequired().HasMaxLength(50);
-            Property(s => s.Email).IsRequired();
-            Property(s => s.Password).IsRequired();
-            HasOptional(s => s.Suggestion).WithRequired().Map(su => su.MapKey("suggestion_id")).WillCascadeOnDelete(false);
+            HasOptional(s => s.Suggestion).WithRequired().Map(su => su.MapKey("suggestion_id"));
         }
     }
 }
---
+++ p2groep04/p2groep04/Models/DAL/Mapper/UserMapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace p2groep04.Models.DAL.Mapper
+{
+    public class UserMapper: EntityTypeConfiguration<User>
+    {
+        public UserMapper()
+        {
+            HasKey(u => u.Id);
+            Property(u => u.FirstName).IsRequired().HasMaxLength(30);
+            Property(u => u.LastName).IsRequired().HasMaxLength(30);
+            Property(u => u.Email).IsRequired().HasMaxLength(50);
+        }
+    }
+}
