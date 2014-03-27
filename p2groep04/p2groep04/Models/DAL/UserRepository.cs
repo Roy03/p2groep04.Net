b5d585c661db72c948f2dc6d138ba0d71a5118bc
Roy Hollanders <roy_9852@hotmail.com>
2014-03-27 09:39:15 +0100

    UserRepository en de Interface aangemaakt en de repositories van Student, Promotor en BPCoordinator verwijderd

---
+++ p2groep04/p2groep04/Models/DAL/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using p2groep04.Models.Domain;
+
+namespace p2groep04.Models.DAL
+{
+    public class UserRepository:IUserRepository
+    {
+        private ProjectContext context;
+        private DbSet<User> users;
+
+        public UserRepository(ProjectContext context)
+        {
+            this.context = context;
+            users = context.Users;
+        }
+
+        public IQueryable<User> FindAll()
+        {
+            return users.OrderBy(u => u.LastName);
+        }
+
+        public User FindBy(int id)
+        {
+            return users.FirstOrDefault(u => u.Id == id);
+        }
+    }
+}
---
+++ p2groep04/p2groep04/Models/Domain/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace p2groep04.Models.Domain
+{
+    public interface IUserRepository
+    {
+        IQueryable<User> FindAll();
+        User FindBy(int id);
+    }
+}
