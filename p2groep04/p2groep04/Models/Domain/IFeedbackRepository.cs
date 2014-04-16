073b87a5ca67b1b7f704ef612b4852c2bd25b57f
BillUnboxedCrate <bram.baert@hotmail.com>
2014-04-16 16:58:11 +0200

    Added feedbackRepository en mapper

---
+++ p2groep04/p2groep04/Models/Domain/IFeedbackRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2groep04.Models.Domain
+{
+    interface IFeedbackRepository
+    {
+        IQueryable<Feedback> FindAll();
+        Feedback FindBy(int id);
+    }
+}
