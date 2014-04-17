d8d35dbf11d9d5952c35ef6b81fddfabb923eb78
BillUnboxedCrate <bram.baert@hotmail.com>
2014-04-17 16:57:38 +0200

    Feedback toegevoegd

---
+++ p2groep04/p2groep04/Feedback.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace p2groep04
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+   
+
+        public String Inhoud { get; set; }
+        
+
+        public int Created_On { get; set; }
+        
+
+        public Boolean Visable { get; set; }
+        
+    }
+}
