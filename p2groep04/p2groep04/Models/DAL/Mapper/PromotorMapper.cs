49567c00c721ecae0b55fd78e2024c7b0ddf85df
208134ld <l.dupont@hotmail.be>
2014-05-14 10:52:04 +0200

    mappers

---
+++ p2groep04/p2groep04/Models/DAL/Mapper/PromotorMapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using p2groep04.Models.Domain;
+
+namespace p2groep04.Models.DAL.Mapper
+{
+    public class PromotorMapper : EntityTypeConfiguration<Promotor>
+    {
+        public PromotorMapper()
+        {  
+
+            HasMany(p => p.Students).WithRequired(p => p.Promotor);
+            ToTable("user");
+        }
+    }
+}
